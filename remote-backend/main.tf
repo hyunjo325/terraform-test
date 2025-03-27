b0c4bc5a8f1972016b6e91d31448e7221a720101
hyunjo325 <hyunjo325@naver.com>
2025-03-27 10:11:48 +0900

    Add remote-backend infrastructure (S3, DynamoDB)

---
+++ remote-backend/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "terraform-evan-tfstate"
+  force_destroy = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "global"
+  }
+}
