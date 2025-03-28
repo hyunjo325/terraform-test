94fac1b24c638cb7b3122145201eb76b23a36035
hyunjo325 <hyunjo325@naver.com>
2025-03-28 16:15:10 +0900

    슬랙알람 추가

--- prod/variables.tf
+++ prod/variables.tf
@@ -1,6 +1,10 @@
 variable "region" {
   default = "ap-northeast-2"
 }
+variable "slack_webhook_url" {
+  description = "Slack Webhook URL"
+  type        = string
+}
 
 #variable "profile" {
 #  default = "default"
